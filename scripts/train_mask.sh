f5c6114c691e412885905609762e08038d0befa3
Mukilan Krishnakumar <mukilan.git@gmail.com>
2023-09-26 13:11:29 +0530

    Images Problem fixed

--- scripts/train_mask.sh
+++ scripts/train_mask.sh
@@ -5,9 +5,9 @@ cd ../tools
 python train_net.py \
     --dataset_name          annots \
     --json_annotation_train annots/train.json \
-    --image_path_train      annots/images \
+    --image_path_train      annots \
     --json_annotation_val   annots/test.json \
-    --image_path_val        annots/images \
+    --image_path_val        annots \
     --config-file           ../configs/prima/mask_rcnn_R_50_FPN_3x.yaml \
     OUTPUT_DIR  ../outputs/prima/mask_rcnn_R_50_FPN_3x/ \
     SOLVER.IMS_PER_BATCH 2 
